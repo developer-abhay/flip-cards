a3926605de5b39b0320286f38f794c942d0021be
Abhay <abhaysharma.developer@gmail.com>
2024-09-04 17:38:09 +0530

    Matching and unflip cards

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
 import "./style.css";
 
-const flipTime = 1000;
-
 const cards = document.querySelectorAll(".card");
 
+// Event Listener
 cards.forEach((card) => {
   card.addEventListener("click", () => {
-    card.classList.add("flip");
-
-    setTimeout(() => {
-      card.classList.remove("flip");
-    }, flipTime);
+    if (!card.classList.contains("flip")) {
+      card.classList.add("flip");
+      match();
+      unflip();
+    }
   });
 });
+
+// Match Values
+const match = () => {
+  const flipCards = document.querySelectorAll(".flip");
+  console.log(flipCards);
+  if (flipCards.length == 2) {
+    const card1 = flipCards[0];
+    const card2 = flipCards[1];
+    const val1 = card1.children[0].innerHTML;
+    const val2 = card2.children[0].innerHTML;
+
+    if (val1 == val2) {
+      setTimeout(() => {
+        card1.style.visibility = "hidden";
+        card2.style.visibility = "hidden";
+      }, 400);
+    }
+  }
+};
+
+// Unflip
+const unflip = () => {
+  const flipCards = document.querySelectorAll(".flip");
+  if (flipCards.length == 2) {
+    setTimeout(() => {
+      flipCards.forEach((card) => {
+        card.classList.remove("flip");
+      });
+    }, 500);
+  }
+
+  if (flipCards.length > 2) {
+    setTimeout(() => {
+      flipCards.forEach((card) => {
+        card.classList.remove("flip");
+      });
+    }, 150);
+  }
+};
