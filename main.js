25730ab5876644bd7fa7f495cfee1139e7dc25f8
Abhay <abhaysharma.developer@gmail.com>
2024-09-04 16:04:16 +0530

    Basic flip cards UI

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const flipTime = 1000;
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    card.classList.add("flip");
+
+    setTimeout(() => {
+      card.classList.remove("flip");
+    }, flipTime);
+  });
+});
